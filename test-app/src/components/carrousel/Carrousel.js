2b3e227379930a2ab117bef264c924b48188751f
Aaron <aaronf09@outlook.com>
2025-02-03 16:51:53 -0600

    Updating  the auction page

--- test-app/src/components/carrousel/Carrousel.js
+++ test-app/src/components/carrousel/Carrousel.js
@@ -1,71 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Carrousel.css";
 
+const images = [
+    "/images/tools.png",
+    "/images/sailor-saturn.png",
+    "/images/pallets.png",
+    "/images/auction.png"
+];
+
 const Carrousel = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const goToPrevious = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
+
+    const goToNext = () => {
+        const isLastSlide = currentIndex === images.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    };
+
     return (
-        <>
-            <section class="carousel" aria-label="Gallery">
-                <ol class="carousel__viewport">
-                    <li id="carousel__slide1"
-                        tabindex="0"
-                        class="carousel__slide">
-                        <div class="carousel__snapper">
-                            <a href="#carousel__slide4"
-                                class="carousel__prev">Go to last slide</a>
-                            <a href="#carousel__slide2"
-                                class="carousel__next">Go to next slide</a>
-                        </div>
-                    </li>
-                    <li id="carousel__slide2"
-                        tabindex="0"
-                        class="carousel__slide">
-                        <div class="carousel__snapper"></div>
-                        <a href="#carousel__slide1"
-                            class="carousel__prev">Go to previous slide</a>
-                        <a href="#carousel__slide3"
-                            class="carousel__next">Go to next slide</a>
-                    </li>
-                    <li id="carousel__slide3"
-                        tabindex="0"
-                        class="carousel__slide">
-                        <div class="carousel__snapper"></div>
-                        <a href="#carousel__slide2"
-                            class="carousel__prev">Go to previous slide</a>
-                        <a href="#carousel__slide4"
-                            class="carousel__next">Go to next slide</a>
-                    </li>
-                    <li id="carousel__slide4"
-                        tabindex="0"
-                        class="carousel__slide">
-                        <div class="carousel__snapper"></div>
-                        <a href="#carousel__slide3"
-                            class="carousel__prev">Go to previous slide</a>
-                        <a href="#carousel__slide1"
-                            class="carousel__next">Go to first slide</a>
-                    </li>
-                </ol>
-                <aside class="carousel__navigation">
-                    <ol class="carousel__navigation-list">
-                        <li class="carousel__navigation-item">
-                            <a href="#carousel__slide1"
-                                class="carousel__navigation-button">Go to slide 1</a>
-                        </li>
-                        <li class="carousel__navigation-item">
-                            <a href="#carousel__slide2"
-                                class="carousel__navigation-button">Go to slide 2</a>
-                        </li>
-                        <li class="carousel__navigation-item">
-                            <a href="#carousel__slide3"
-                                class="carousel__navigation-button">Go to slide 3</a>
-                        </li>
-                        <li class="carousel__navigation-item">
-                            <a href="#carousel__slide4"
-                                class="carousel__navigation-button">Go to slide 4</a>
-                        </li>
-                    </ol>
-                </aside>
-            </section>
-        </>
+        <div className="carousel">
+            <button onClick={goToPrevious} className="carousel__button carousel__button--left">‹</button>
+            <div className="carousel__image-container">
+                <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="carousel__image" />
+            </div>
+            <button onClick={goToNext} className="carousel__button carousel__button--right">›</button>
+        </div>
     );
 };
 
